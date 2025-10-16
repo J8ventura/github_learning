264d0c7194758a654fd2e04aeabc83e4a65aed89
j8ventura <j8ventura@gmail.com>
2025-10-16 15:20:35 -0500

    updated notes

--- github_learning/test.py
+++ github_learning/test.py
@@ -3,25 +3,27 @@
 
 #STEP 1:LOGIN--gh auth login
 
-#STEP 2:REPOSITORY
-#create a repo(folder with files--project)
-
 #STEP 2:INITIALIZE--git init
 #starts up the repo in github
 
-#STEP 3:STAGE--git add "filename"
+#STEP 3:STAGE--git add . ("." stages all in current folder. "filename" for specific files)
 #chooses which files you want for the save point
 
 #STEP 4:CONFIRM--git status
 #verify it worked
 
-#STEP 4:COMMIT--git commit -m "message"
+#STEP 5:COMMIT--git commit -m "message"
 #creates save point(snapshot) locally w/message
 
-#STEP 5:PUSH--git push
-#uploads to github
+#STEP 6:REPOSITORY--git remote add origin <url>
+#CREATE REPO IN GITHUB FIRST
+#connects your local repo to repo in github 
 
-#STEP 6:PULL--git pull
+#STEP 7:PUSH--git push *(if 1st time: git push -u origin master)
+#uploads commit to github
+
+#STEP 8:PULL--git pull
 #downloads latest from github
 
 print("testing basic steps")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
