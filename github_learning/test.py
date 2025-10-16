97b6633739e760967447aeb88351e3f555fcb60e
j8ventura <j8ventura@gmail.com>
2025-10-16 14:33:50 -0500

    first stage test

---
+++ github_learning/test.py
@@ -0,0 +1,27 @@
+
+#BASIC GITHUB COMMANDS--POWERSHELL TERMINAL
+
+#STEP 1:LOGIN--gh auth login
+
+#STEP 2:REPOSITORY
+#create a repo(folder with files--project)
+
+#STEP 2:INITIALIZE--git init
+#starts up the repo in github
+
+#STEP 3:STAGE--git add "filename"
+#chooses which files you want for the save point
+
+#STEP 4:CONFIRM--git status
+#verify it worked
+
+#STEP 4:COMMIT--git commit -m "message"
+#creates save point(snapshot) locally w/message
+
+#STEP 5:PUSH--git push
+#uploads to github
+
+#STEP 6:PULL--git pull
+#downloads latest from github
+
+print("testing basic steps")
